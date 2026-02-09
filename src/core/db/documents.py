6bacda03f832ad5dc3d72e14136451c989862bfd
Shayantrix <shayantrixytb@gmail.com>
2026-02-09 12:08:48 +0330

    mongodb is done

--- src/core/db/documents.py
+++ src/core/db/documents.py
@@ -22,3 +22,85 @@ class BaseDocument(BaseModel):
         return cls(**dict(data, id=id))
 
     def to_mongo(self, **kwargs) -> dict:
+        """Convert "id" (UUID object) into "_id" (str object)"""
+        exclude_unset = kwargs.pop("exclude_unset", False)
+        by_alias = kwargs.pop("by_alias", True)
+
+        parsed = self.model_dump(
+            exclude_unset=exclude_unset,
+            by_alias=by_alias,
+            **kwargs
+        )
+
+        if "_id" not in parsed and "id" in parsed:
+            parsed["_id"] = str(parsed.pop("id"))
+
+        return parsed
+
+    def save(self, **kwargs):
+        collection = _database[self._get_collection_name()]
+
+        try:
+            result = collection.insert_one(self.to_mongo(**kwargs))
+            return result.inserted_id
+        except errors.WriteError:
+            print("Error saving document")
+            return None
+
+
+    @classmethod
+    def _get_collection_name(cls):
+        if not hasattr(cls, "Settings") or not hasattr(cls.Settings, "name"):
+            raise ImproperlyConfigured(
+                "Document should define an Settings configuration class with the name of the collection"
+            )
+        return cls.Settings.name
+
+    @classmethod
+    def get_or_create(cls, **filter_options) -> Optional[str]:
+        collection = _database[cls._get_collection_name()]
+        try:
+            instance = collection.find_one(filter_options)
+            if instance:
+                return str(cls.from_mongo(instance).id)
+            new_instance = cls(**filter_options)
+            new_instance = new_instance.save()
+            return new_instance
+        except errors.WriteError:
+            print("failed to retrieve document or create one")
+            return None
+
+    @classmethod
+    def find(cls, **filter_options):
+        collection = _database[cls._get_collection_name()]
+        try:
+            instance = collection.find_one(filter_options)
+            if instance:
+                return cls.from_mongo(instance)
+
+            return None
+        except errors.OperationFailure:
+            print("failed to retrieve document")
+            return None
+
+    @classmethod
+    def multi_insert(cls, documents: List, **kwargs) -> Optional[List[str]]:
+        collection = _database[cls._get_collection_name()]
+        try:
+            result = collection.insert_many(
+                [doc.to_mongo(**kwargs) for doc in documents]
+            )
+            return result.inserted_ids
+        except errors.WriteError:
+            print("failed to insert documents")
+            return None
+
+
+class ArticleDocument(BaseDocument):
+    platform: str
+    link: str
+    content: str
+    author_id: str = Field(alias="author_id")
+
+    class Settings:
+        name = "articles"
